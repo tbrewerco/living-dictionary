7323714bdf54c0ce00b17def9005e77e08e5ade4
Travis Brewer <travis@Traviss-MacBook-Pro.local>
2021-05-24 13:23:50 -0600

    begin building javascript/html

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
-// // verifying js is connected to html
-// alert('js!')
-// // verifying jQuery is accessible in this file
-// console.log($)
-
-/*
-PSEUDOCODE
-
-// on page load, get data from api
-// store data in global state variable
-// use data to build user interface and add elements to the DOM
-*/
-
 
 // state variables________
-// cached element references________
-// event listeners________
+let wordData;
+
+// variables
+let searchText = $("input").val(); // store user input in variable
+
 // functions________
+function render() {
+
+}
+
+function handleGetData(event) { // declare handleGetData function, to be called on form submit
+
+    event.preventDefault(); // prevents page refresh on form submit
+
+    $.ajax({ // gets data from dictonaryapi.com using user input (searchText) as keyword
+            url: `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchText}?key=ae49d360-70d0-4fb5-9a08-fa08c61a5b33`
+        })
+        .then(function (data) { // executes when data received 
+                wordData = data;
+                render(); // calls render function
+            },
+            function (error) {  // alerts error
+                alert("bad request: ", error);
+            });
+}
 
 
-// TO DO
-
+// event listeners________
+$('form').on('submit', handleGetData); // calls handleGetData on form submit
