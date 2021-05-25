86e7fa0b0a850d0cd7f1b8ebb565b0ab184dab2f
Travis Brewer <travis@Traviss-MacBook-Pro.local>
2021-05-25 11:23:00 -0600

    Minimum functionality achieved in JS/HTML files

--- js/script.js
+++ js/script.js
@@ -1,39 +1,50 @@
-
 // state variables________
 let wordData;
 
-// variables
-$word = $(".word"); // target specific classes and ids
+// variables________
 
-$pronunciation = $(".pronunciation")
-$partOfSpeech = $(".partOfSpeech")
-$defItemOne = $("#defItemOne")
-$defsOne = $("#defsOne")
-$defItemTwo = $("#defItemTwo")
-$partOfSpeechTwo = $(".partOfSpeechTwo")
-// $defsTwo = $("#defsTwo")
-// $similarWordsList = $("similarWordsList")
+$main = $("main")
+$word = $("<h2>"); // sets variable to h2 element
+$pronunciation = $("<h3>") // sets variable to h3 element
+$partOfSpeech = $("<h4>") // sets variable to h4 element
+$defsList = $("<ul>") // sets variable to ul element
+$definition = $("<div>") // sets variable to div element
 
 // functions________
 function render() {
-console.log(wordData);
-$word.text(wordData[0].hwi.hw);
-$pronunciation.text(wordData[0].hwi.prs[0].mw);
-$partOfSpeech.text(wordData[0].fl)
-$defItemOne.text(wordData[0].shortdef[0])
-$defsOne.append(`<li>${wordData[0].shortdef[1]}</li>`)
 
-$partOfSpeechTwo.text(wordData[1].fl)
+    $main.empty(); // clears main content
+    $defsList.empty(); // clears list content
 
+    $definition.addClass('definition'); // adds class to definition div
+    $mainContent.append($definition); // appends definition div to main
+
+    $word.addClass("word"); // adds class to word h2
+    $word.text(wordData[0].hwi.hw); // set text of $word h2 to object item
+    $definition.append($word); // append word to $definition div
+
+    $pronunciation.addClass("pronunciation"); // adds class to pronunciation h3
+    $pronunciation.text(wordData[0].hwi.prs[0].mw); // set text of pronunciation to object item
+    $definition.append($pronunciation) // append to $definition div
+
+    $partOfSpeech.addClass("partOfSpeech"); // adds class to partOfSpeech h4
+    $partOfSpeech.text(wordData[0].fl); // set text to object item
+    $definition.append($partOfSpeech); // append to definition div
+
+    $definition.append($defsList) // appends defsList (unordered list) to definition div
+
+    wordData[0].shortdef.forEach((definition) => { // arrow function that, for each definition in the object, creates a list item element and displays the definition text in the list item
+        // console.log(definition) TEST
+        $li = $("<li>")
+        $li.text(definition)
+        $defsList.append($li);
+    })
 
 };
 
 function handleGetData(event) { // declare handleGetData function, to be called on form submit
-
     event.preventDefault(); // prevents page refresh on form submit
-
-    let searchText = $("input").val() // store user input in variable
-
+    let searchText = $("input").val() // store user input in variable searchText
     $.ajax( // gets data from dictonaryapi.com using user input (searchText) as keyword
             `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchText}?key=ae49d360-70d0-4fb5-9a08-fa08c61a5b33`
         )
@@ -41,12 +52,11 @@ function handleGetData(event) { // declare handleGetData function, to be called 
                 wordData = data;
                 render(); // calls render function
             },
-            function (error) {  // alerts error
+            function (error) { // alerts error
                 alert("bad request: ", error);
             });
-            console.log(searchText)
+    console.log(searchText)
 }
-
 
 // event listeners________
 $('form').on('submit', handleGetData); // calls handleGetData on form submit
